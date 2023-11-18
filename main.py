6131d1edd5099ead5ee07d751f11bf2321850926
Ketan Patil <Ketanp132134@gmail.com>
2023-11-18 19:44:31 +0530

    clean startup data

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-df = pd.read_csv('startup_funding.csv')
+df = pd.read_csv('startup_funding_clean.csv')
 
 
 st.title('Indian Startup Funding Analysis')
@@ -13,10 +13,10 @@ if option == 'Overall Analysis':
     st.header('Overall Analysis')
 elif option == 'Startup':
     st.header('Startup Analysis')
-    st.sidebar.selectbox('Select Startup',['Byjus','Ola'])
+    st.sidebar.selectbox('Select Startup',sorted(df.startup))
 else:
     st.header('Investor Analysis')
-    st.sidebar.selectbox('Select Investor',['p1','p2'])
+    st.sidebar.selectbox('Select Investor',sorted(set(df.investors.str.split(',').sum())))
 
 
 
